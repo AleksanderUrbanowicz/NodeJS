b4c55af3933597383c7e13ae10fec15b7f238728
AleksanderUrbanowicz <aleksanderurbanowicz@gmail.com>
2020-07-02 02:02:56 +0200

    events

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 //This is commmonJS, import would be ES6, not implemented in node yet
 const Person = require('./person'); 
-
+const Logger = require('./logger');
 
 const person1 = new Person('John Doe',30);
 person1.greeting();
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called listener:', data ));
+logger.log('msg argument');
